6b8b4dc590af366d08c1adf16993a36607717486
PEHHUU <156270835+PEHHUU@users.noreply.github.com>
2024-01-18 09:51:23 +0900

    6_header 완성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import HomeView from './views/HomeView';
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element ={<HomeView />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
