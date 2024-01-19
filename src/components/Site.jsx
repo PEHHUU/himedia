96a09ffff29e3ce30485e1fcdee96594af180f38
PEHHUU <156270835+PEHHUU@users.noreply.github.com>
2024-01-19 11:35:04 +0900

    9_ site 완성

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
-
+const siteText = [
+  {
+    text :["Make","site compliant with","webstardard"],
+    title :"웹 표준을 준수한 사이트 제작",
+    site :["code","code"],
+    info :["site code","produiction period : two days","use stack : html5/css javascript"]
+  },
+  {
+    text :["design Make","design site compliant with","ui/ux design"],
+    title :["웹 표준을 준수한 사이트 제작"],
+    site :"https://github.com/PEHHUU/himedia",
+    info :["site code","produiction period : two days","use stack : photooshop, illust, 피그마, 깃허브"]
+  },
+  {
+    text :["design Make","design site compliant with","ui/ux design"],
+    title :["웹 표준을 준수한 사이트 제작"],
+    site :["code","code"],
+    info :["site code","produiction period : two days","use stack : photooshop, illust, 피그마, 깃허브"]
+  },
+  {
+    text :["design Make","design site compliant with","ui/ux design"],
+    title :["웹 표준을 준수한 사이트 제작"],
+    site :["code","code"],
+    info :["site code","produiction period : two days","use stack : photooshop, illust, 피그마, 깃허브"]
+  },
+]
 const Site = () => {
   return (
-    <div>
-      Site
-    </div>
+    <section id='site'>
+      <div className="site_inner">
+        <div className="site_title">
+          Site coding <em>작품들</em>
+        </div>
+        <div className="site_wrap">
+     {siteText.map((site,key)=>(
+                <article className={`site_items s${key + 1}`}key={key} >
+                <span className='num'>0{key + 1}.</span>
+                <div className="text">
+                  <div>{site.text[0]}</div>
+                  <div>{site.text[1]}</div>
+                  <div>{site.text[2]}</div>
+                </div>
+                <h3 className='title'>{site.title}</h3>
+                <div className="btn">
+                  <a href={site.site}>code</a>
+                  <a href={site.site}>view</a>
+                </div>
+                <div className="info">
+                  <span>{site.info[0]}</span>
+                  <span>{site.info[1]}</span>
+                  <span>{site.info[2]}</span>
+                </div>
+              </article>
+     ))}
+
+        </div>
+      </div>
+    </section>
   )
 }
 
